4f3fa4247092cc97b54b62ce17f89d05c438bafb
Liyog-dev <liyogbartoos@gmail.com>
2025-08-07 00:46:01 +0100

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,1 +1,11 @@
+// config/supabase.js ✅
+
+import { createClient } from "@supabase/supabase-js";
+
+// Supabase Project Details
+const SUPABASE_URL = "https://snwwlewjriuqrodpjhry.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNud3dsZXdqcml1cXJvZHBqaHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDY3MDAsImV4cCI6MjA2ODE4MjcwMH0.WxOmEHxLcEHmMKFjsgrzcb22mPs-sJwW_G3GOuXX2c8";
+
+// Create and export Supabase client
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
