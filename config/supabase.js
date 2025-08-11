d0f30079019f781eb4374c68d70e3b7f635d7ed1
Liyog-dev <liyogbartoos@gmail.com>
2025-08-11 04:01:34 +0100

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,11 +1,7 @@
-// config/supabase.js ✅
+// /config/supabase.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-import { createClient } from "@supabase/supabase-js";
+const SUPABASE_URL = 'https://snwwlewjriuqrodpjhry.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNud3dsZXdqcml1cXJvZHBqaHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDY3MDAsImV4cCI6MjA2ODE4MjcwMH0.WxOmEHxLcEHmMKFjsgrzcb22mPs-sJwW_G3GOuXX2c8';
 
-// Supabase Project Details
-const SUPABASE_URL = "https://snwwlewjriuqrodpjhry.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNud3dsZXdqcml1cXJvZHBqaHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDY3MDAsImV4cCI6MjA2ODE4MjcwMH0.WxOmEHxLcEHmMKFjsgrzcb22mPs-sJwW_G3GOuXX2c8";
-
-// Create and export Supabase client
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
