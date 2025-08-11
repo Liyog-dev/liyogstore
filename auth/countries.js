8c555cf1ccc5f65fd4e5ec0b66d5c36b2d2e74a4
Liyog-dev <liyogbartoos@gmail.com>
2025-08-11 08:57:51 +0100

    Update countries.js

--- auth/countries.js
+++ auth/countries.js
@@ -39,7 +39,8 @@ export const countries = [
     code: "AU",
     states: [
       "New South Wales", "Queensland", "South Australia",
-      "Tasmania", "Victoria", "Western Australia", "Australian Capital Territory", "Northern Territory"
+      "Tasmania", "Victoria", "Western Australia",
+      "Australian Capital Territory", "Northern Territory"
     ]
   },
   {
@@ -54,16 +55,327 @@ export const countries = [
       "Uttarakhand", "West Bengal", "Delhi", "Puducherry", "Chandigarh"
     ]
   },
-  // Add more countries below (100+ list can be expanded)
-  { name: "Germany", code: "DE", states: ["Bavaria", "Berlin", "Brandenburg", "Bremen", "Hamburg", "Hesse", "Lower Saxony", "Mecklenburg-Vorpommern", "North Rhine-Westphalia", "Rhineland-Palatinate", "Saarland", "Saxony", "Saxony-Anhalt", "Schleswig-Holstein", "Thuringia"] },
-  { name: "France", code: "FR", states: ["Île-de-France", "Provence-Alpes-Côte d'Azur", "Nouvelle-Aquitaine", "Occitanie", "Auvergne-Rhône-Alpes", "Brittany", "Normandy", "Hauts-de-France", "Pays de la Loire", "Centre-Val de Loire", "Corsica"] },
-  { name: "Nigeria", code: "NG", states: ["Abia", "Adamawa", "Akwa Ibom", "Anambra", "Bauchi", "Bayelsa", "Benue", "Borno", "Cross River", "Delta", "Ebonyi", "Edo", "Ekiti", "Enugu", "Gombe", "Imo", "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nasarawa", "Niger", "Ogun", "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara", "FCT"] },
-  { name: "Brazil", code: "BR", states: ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"] },
-  { name: "Japan", code: "JP", states: ["Hokkaido", "Tohoku", "Kanto", "Chubu", "Kinki", "Chugoku", "Shikoku", "Kyushu"] }
-  // ... continue adding until you hit 100+ entries
-];
+  {
+    name: "Nigeria",
+    code: "NG",
+    states: [
+      "Abia", "Adamawa", "Akwa Ibom", "Anambra", "Bauchi", "Bayelsa", "Benue",
+      "Borno", "Cross River", "Delta", "Ebonyi", "Edo", "Ekiti", "Enugu",
+      "Gombe", "Imo", "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi",
+      "Kwara", "Lagos", "Nasarawa", "Niger", "Ogun", "Ondo", "Osun", "Oyo",
+      "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara", "FCT"
+    ]
+  },
+  {
+    name: "Brazil",
+    code: "BR",
+    states: [
+      "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará",
+      "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão",
+      "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará",
+      "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro",
+      "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima",
+      "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"
+    ]
+  },
+  {
+    name: "Germany",
+    code: "DE",
+    states: [
+      "Baden-Württemberg", "Bavaria", "Berlin", "Brandenburg", "Bremen",
+      "Hamburg", "Hesse", "Lower Saxony", "Mecklenburg-Vorpommern",
+      "North Rhine-Westphalia", "Rhineland-Palatinate", "Saarland",
+      "Saxony", "Saxony-Anhalt", "Schleswig-Holstein", "Thuringia"
+    ]
+  },
+  {
+    name: "France",
+    code: "FR",
+    states: [
+      "Île-de-France", "Provence-Alpes-Côte d'Azur", "Nouvelle-Aquitaine",
+      "Occitanie", "Auvergne-Rhône-Alpes", "Brittany", "Normandy",
+      "Hauts-de-France", "Pays de la Loire", "Centre-Val de Loire", "Corsica"
+    ]
+  },
+  {
+    name: "Japan",
+    code: "JP",
+    states: [
+      "Hokkaido", "Aomori", "Iwate", "Miyagi", "Akita", "Yamagata", "Fukushima",
+      "Ibaraki", "Tochigi", "Gunma", "Saitama", "Chiba", "Tokyo", "Kanagawa",
+      "Niigata", "Toyama", "Ishikawa", "Fukui", "Yamanashi", "Nagano",
+      "Gifu", "Shizuoka", "Aichi", "Mie", "Shiga", "Kyoto", "Osaka",
+      "Hyogo", "Nara", "Wakayama", "Tottori", "Shimane", "Okayama",
+      "Hiroshima", "Yamaguchi", "Tokushima", "Kagawa", "Ehime", "Kochi",
+      "Fukuoka", "Saga", "Nagasaki", "Kumamoto", "Oita", "Miyazaki", "Kagoshima", "Okinawa"
+    ]
+  },
+  {
+    name: "Mexico",
+    code: "MX",
+    states: [
+      "Aguascalientes", "Baja California", "Baja California Sur", "Campeche",
+      "Chiapas", "Chihuahua", "Coahuila", "Colima", "Durango", "Guanajuato",
+      "Guerrero", "Hidalgo", "Jalisco", "México", "Michoacán", "Morelos",
+      "Nayarit", "Nuevo León", "Oaxaca", "Puebla", "Querétaro", "Quintana Roo",
+      "San Luis Potosí", "Sinaloa", "Sonora", "Tabasco", "Tamaulipas",
+      "Tlaxcala", "Veracruz", "Yucatán", "Zacatecas"
+    ]
+  },
+  {
+    name: "Italy",
+    code: "IT",
+    states: [
+      "Abruzzo", "Aosta Valley", "Apulia", "Basilicata", "Calabria",
+      "Campania", "Emilia-Romagna", "Friuli Venezia Giulia", "Lazio",
+      "Liguria", "Lombardy", "Marche", "Molise", "Piedmont", "Sardinia",
+      "Sicily", "Trentino-Alto Adige/Südtirol", "Tuscany", "Umbria",
+      "Veneto"
+    ]
+  },
+  {
+    name: "Spain",
+    code: "ES",
+    states: [
+      "Andalusia", "Aragon", "Asturias", "Balearic Islands", "Basque Country",
+      "Canary Islands", "Cantabria", "Castile and León", "Castile-La Mancha",
+      "Catalonia", "Extremadura", "Galicia", "La Rioja", "Madrid",
+      "Murcia", "Navarre", "Valencian Community"
+    ]
+  },
+  {
+    name: "Russia",
+    code: "RU",
+    states: [
+      "Adygea", "Altai Republic", "Bashkortostan", "Buryatia", "Chechnya",
+      "Chuvashia", "Dagestan", "Ingushetia", "Kabardino-Balkaria",
+      "Kalmykia", "Karachay-Cherkessia", "Karelia", "Khakassia",
+      "Komi Republic", "Mari El", "Mordovia", "North Ossetia–Alania",
+      "Sakha (Yakutia)", "Tatarstan", "Tuva", "Udmurtia",
+      "Altai Krai", "Kamchatka Krai", "Khabarovsk Krai", "Krasnodar Krai",
+      "Krasnoyarsk Krai", "Perm Krai", "Primorsky Krai", "Stavropol Krai",
+      "Zabaykalsky Krai", "Amur Oblast", "Arkhangelsk Oblast",
+      "Astrakhan Oblast", "Belgorod Oblast", "Bryansk Oblast",
+      "Chelyabinsk Oblast", "Irkutsk Oblast", "Ivanovo Oblast",
+      "Kaliningrad Oblast", "Kaluga Oblast", "Kemerovo Oblast",
+      "Kirov Oblast", "Kostroma Oblast", "Kurgan Oblast", "Kursk Oblast",
+      "Leningrad Oblast", "Lipetsk Oblast", "Magadan Oblast",
+      "Moscow Oblast", "Murmansk Oblast", "Nizhny Novgorod Oblast",
+      "Novgorod Oblast", "Novosibirsk Oblast", "Omsk Oblast",
+      "Orenburg Oblast", "Oryol Oblast", "Penza Oblast",
+      "Pskov Oblast", "Rostov Oblast", "Ryazan Oblast", "Sakhalin Oblast",
+      "Samara Oblast", "Saratov Oblast", "Smolensk Oblast",
+      "Sverdlovsk Oblast", "Tambov Oblast", "Tomsk Oblast",
+      "Tula Oblast", "Tver Oblast", "Tyumen Oblast", "Ulyanovsk Oblast",
+      "Vladimir Oblast", "Volgograd Oblast", "Vologda Oblast",
+      "Voronezh Oblast", "Yaroslavl Oblast", "Moscow City",
+      "Saint Petersburg City", "Sevastopol", "Jewish Autonomous Oblast",
+      "Nenets Autonomous Okrug", "Khanty-Mansi Autonomous Okrug",
+      "Chukotka Autonomous Okrug", "Yamalo-Nenets Autonomous Okrug"
+    ]
+  },
+  {
+    name: "China",
+    code: "CN",
+    states: [
+      "Anhui", "Beijing", "Chongqing", "Fujian", "Gansu", "Guangdong",
+      "Guangxi", "Guizhou", "Hainan", "Hebei", "Heilongjiang",
+      "Henan", "Hubei", "Hunan", "Inner Mongolia", "Jiangsu",
+      "Jiangxi", "Jilin", "Liaoning", "Ningxia", "Qinghai",
+      "Shaanxi", "Shandong", "Shanghai", "Shanxi", "Sichuan",
+      "Tianjin", "Tibet", "Xinjiang", "Yunnan", "Zhejiang"
+    ]
+  },
+  {
+    name: "Argentina",
+    code: "AR",
+    states: [
+      "Buenos Aires", "Catamarca", "Chaco", "Chubut", "Córdoba",
+      "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa",
+      "La Rioja", "Mendoza", "Misiones", "Neuquén", "Río Negro",
+      "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe",
+      "Santiago del Estero", "Tierra del Fuego", "Tucumán"
+    ]
+  },
+  {
+    name: "Saudi Arabia",
+    code: "SA",
+    states: [
+      "Riyadh", "Makkah", "Madinah", "Qassim", "Eastern Province",
+      "Asir", "Tabuk", "Hail", "Northern Borders", "Jazan",
+      "Najran", "Al Bahah", "Al Jawf"
+    ]
+  },
+  {
+    name: "Indonesia",
+    code: "ID",
+    states: [
+      "Aceh", "Bali", "Banten", "Bengkulu", "Central Java",
+      "Central Kalimantan", "Central Sulawesi", "East Java",
+      "East Kalimantan", "East Nusa Tenggara", "Gorontalo",
+      "Jakarta", "Jambi", "Lampung", "Maluku", "North Kalimantan",
+      "North Maluku", "North Sulawesi", "North Sumatra",
+      "Papua", "Riau", "Riau Islands", "Southeast Sulawesi",
+      "South Kalimantan", "South Sulawesi", "South Sumatra",
+      "West Java", "West Kalimantan", "West Nusa Tenggara",
+      "West Papua", "West Sulawesi", "West Sumatra", "Yogyakarta"
+    ]
+  },
+  {
+    name: "Pakistan",
+    code: "PK",
+    states: [
+      "Balochistan", "Khyber Pakhtunkhwa", "Punjab", "Sindh",
+      "Islamabad Capital Territory", "Gilgit-Baltistan", "Azad Jammu and Kashmir"
+    ]
+  },
 
-// ============================
+  {
+    name: "South Africa",
+    code: "ZA",
+    states: [
+      "Eastern Cape", "Free State", "Gauteng", "KwaZulu-Natal",
+      "Limpopo", "Mpumalanga", "Northern Cape", "North West", "Western Cape"
+    ]
+  },
+  {
+    name: "Kenya",
+    code: "KE",
+    states: [
+      "Baringo", "Bomet", "Bungoma", "Busia", "Elgeyo-Marakwet", "Embu",
+      "Garissa", "Homa Bay", "Isiolo", "Kajiado", "Kakamega", "Kericho",
+      "Kiambu", "Kilifi", "Kirinyaga", "Kisii", "Kisumu", "Kitui", "Kwale",
+      "Laikipia", "Lamu", "Machakos", "Makueni", "Mandera", "Marsabit",
+      "Meru", "Migori", "Mombasa", "Murang'a", "Nairobi", "Nakuru",
+      "Nandi", "Narok", "Nyamira", "Nyandarua", "Nyeri", "Samburu",
+      "Siaya", "Taita-Taveta", "Tana River", "Tharaka-Nithi", "Trans Nzoia",
+      "Turkana", "Uasin Gishu", "Vihiga", "Wajir", "West Pokot"
+    ]
+  },
+  {
+    name: "Ethiopia",
+    code: "ET",
+    states: [
+      "Addis Ababa", "Afar", "Amhara", "Benishangul-Gumuz", "Dire Dawa",
+      "Gambela", "Harari", "Oromia", "Sidama", "Somali", "Southern Nations, Nationalities, and Peoples' Region", "Tigray", "South West Ethiopia Peoples Region"
+    ]
+  },
+  {
+    name: "Egypt",
+    code: "EG",
+    states: [
+      "Alexandria", "Aswan", "Asyut", "Beheira", "Beni Suef", "Cairo",
+      "Dakahlia", "Damietta", "Faiyum", "Gharbia", "Giza", "Ismailia",
+      "Kafr el-Sheikh", "Luxor", "Matrouh", "Minya", "Monufia",
+      "New Valley", "North Sinai", "Port Said", "Qalyubia", "Qena",
+      "Red Sea", "Sharqia", "Sohag", "South Sinai", "Suez"
+    ]
+  },
+  {
+    name: "Turkey",
+    code: "TR",
+    states: [
+      "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya",
+      "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir",
+      "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis",
+      "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum",
+      "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan",
+      "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane",
+      "Hakkari", "Hatay", "Iğdır", "Isparta", "Istanbul", "İzmir",
+      "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu",
+      "Kayseri", "Kilis", "Kırıkkale", "Kırklareli", "Kırşehir",
+      "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Mardin",
+      "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye",
+      "Rize", "Sakarya", "Samsun", "Şanlıurfa", "Siirt", "Sinop",
+      "Şırnak", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli",
+      "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak"
+    ]
+  },
+  {
+    name: "Philippines",
+    code: "PH",
+    states: [
+      "Abra", "Agusan del Norte", "Agusan del Sur", "Aklan", "Albay",
+      "Antique", "Apayao", "Aurora", "Basilan", "Bataan", "Batanes",
+      "Batangas", "Benguet", "Biliran", "Bohol", "Bukidnon", "Bulacan",
+      "Cagayan", "Camarines Norte", "Camarines Sur", "Camiguin",
+      "Capiz", "Catanduanes", "Cavite", "Cebu", "Cotabato", "Davao de Oro",
+      "Davao del Norte", "Davao del Sur", "Davao Occidental", "Davao Oriental",
+      "Dinagat Islands", "Eastern Samar", "Guimaras", "Ifugao", "Ilocos Norte",
+      "Ilocos Sur", "Iloilo", "Isabela", "Kalinga", "La Union",
+      "Laguna", "Lanao del Norte", "Lanao del Sur", "Leyte",
+      "Maguindanao del Norte", "Maguindanao del Sur", "Marinduque",
+      "Masbate", "Misamis Occidental", "Misamis Oriental", "Mountain Province",
+      "Negros Occidental", "Negros Oriental", "Northern Samar", "Nueva Ecija",
+      "Nueva Vizcaya", "Occidental Mindoro", "Oriental Mindoro", "Palawan",
+      "Pampanga", "Pangasinan", "Quezon", "Quirino", "Rizal", "Romblon",
+      "Samar", "Sarangani", "Siquijor", "Sorsogon", "South Cotabato",
+      "Southern Leyte", "Sultan Kudarat", "Sulu", "Surigao del Norte",
+      "Surigao del Sur", "Tarlac", "Tawi-Tawi", "Zambales", "Zamboanga del Norte",
+      "Zamboanga del Sur", "Zamboanga Sibugay"
+    ]
+  },
+  {
+    name: "Malaysia",
+    code: "MY",
+    states: [
+      "Johor", "Kedah", "Kelantan", "Malacca", "Negeri Sembilan", "Pahang",
+      "Penang", "Perak", "Perlis", "Sabah", "Sarawak", "Selangor", "Terengganu",
+      "Kuala Lumpur", "Labuan", "Putrajaya"
+    ]
+  },
+  {
+    name: "Thailand",
+    code: "TH",
+    states: [
+      "Amnat Charoen", "Ang Thong", "Bangkok", "Bueng Kan", "Buri Ram",
+      "Chachoengsao", "Chai Nat", "Chaiyaphum", "Chanthaburi", "Chiang Mai",
+      "Chiang Rai", "Chon Buri", "Chumphon", "Kalasin", "Kamphaeng Phet",
+      "Kanchanaburi", "Khon Kaen", "Krabi", "Lampang", "Lamphun",
+      "Loei", "Lop Buri", "Mae Hong Son", "Maha Sarakham", "Mukdahan",
+      "Nakhon Nayok", "Nakhon Pathom", "Nakhon Phanom", "Nakhon Ratchasima",
+      "Nakhon Sawan", "Nakhon Si Thammarat", "Nan", "Narathiwat", "Nong Bua Lam Phu",
+      "Nong Khai", "Nonthaburi", "Pathum Thani", "Pattani", "Phang Nga",
+      "Phatthalung", "Phayao", "Phetchabun", "Phetchaburi", "Phichit",
+      "Phitsanulok", "Phra Nakhon Si Ayutthaya", "Phrae", "Phuket", "Prachin Buri",
+      "Prachuap Khiri Khan", "Ranong", "Ratchaburi", "Rayong", "Roi Et",
+      "Sa Kaeo", "Sakon Nakhon", "Samut Prakan", "Samut Sakhon", "Samut Songkhram",
+      "Saraburi", "Satun", "Sing Buri", "Sisaket", "Songkhla", "Sukhothai",
+      "Suphan Buri", "Surat Thani", "Surin", "Tak", "Trang", "Trat", "Ubon Ratchathani",
+      "Udon Thani", "Uthai Thani", "Uttaradit", "Yala", "Yasothon"
+    ]
+  },
+  {
+    name: "Vietnam",
+    code: "VN",
+    states: [
+      "An Giang", "Ba Ria-Vung Tau", "Bac Giang", "Bac Kan", "Bac Lieu", "Bac Ninh",
+      "Ben Tre", "Binh Dinh", "Binh Duong", "Binh Phuoc", "Binh Thuan", "Ca Mau",
+      "Can Tho", "Cao Bang", "Da Nang", "Dak Lak", "Dak Nong", "Dien Bien",
+      "Dong Nai", "Dong Thap", "Gia Lai", "Ha Giang", "Ha Nam", "Ha Noi", "Ha Tinh",
+      "Hai Duong", "Hai Phong", "Hau Giang", "Ho Chi Minh City", "Hoa Binh", "Hung Yen",
+      "Khanh Hoa", "Kien Giang", "Kon Tum", "Lai Chau", "Lam Dong", "Lang Son",
+      "Lao Cai", "Long An", "Nam Dinh", "Nghe An", "Ninh Binh", "Ninh Thuan",
+      "Phu Tho", "Phu Yen", "Quang Binh", "Quang Nam", "Quang Ngai", "Quang Ninh",
+      "Quang Tri", "Soc Trang", "Son La", "Tay Ninh", "Thai Binh", "Thai Nguyen",
+      "Thanh Hoa", "Thua Thien-Hue", "Tien Giang", "Tra Vinh", "Tuyen Quang",
+      "Vinh Long", "Vinh Phuc", "Yen Bai"
+    ]
+  },
+  {
+    name: "Iran",
+    code: "IR",
+    states: [
+      "Alborz", "Ardabil", "Bushehr", "Chaharmahal and Bakhtiari", "East Azerbaijan",
+      "Esfahan", "Fars", "Gilan", "Golestan", "Hamadan", "Hormozgan", "Ilam",
+      "Kerman", "Kermanshah", "Khuzestan", "Kohgiluyeh and Boyer-Ahmad", "Kurdistan",
+      "Lorestan", "Markazi", "Mazandaran", "North Khorasan", "Qazvin", "Qom",
+      "Razavi Khorasan", "Semnan", "Sistan and Baluchestan", "South Khorasan",
+      "Tehran", "West Azerbaijan", "Yazd", "Zanjan"
+    ]
+  }
+
+  // ============================
 // Populate Country Select
 // ============================
 export function populateCountries(selectEl) {
